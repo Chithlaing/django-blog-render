efa1b2ee7a46a28734f9b1b5ca1dc813e5985b23
Chit Hlaing <chithlaingwin.letpan@gmail.com>
2025-12-30 12:12:25 +0630

    Enable media upload

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='profile_pics/')
+    image = models.ImageField(upload_to='profile_pics/', default='profile_pics/default.jpg')
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
