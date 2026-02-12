95164072928d9f4ed53bcdb52479f42811efd38e
Chit Hlaing <chithlaingwin.letpan@gmail.com>
2026-02-12 10:24:18 +0630

    remove pytz

--- django_project/settings.py
+++ django_project/settings.py
@@ -16,7 +16,6 @@ import os
 from pathlib import Path
 import dj_database_url
 from dotenv import load_dotenv
-import pytz
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
